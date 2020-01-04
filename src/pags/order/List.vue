348e63965425d270f9ad843028d4550f8aa151b5
Sun9902 <1596459929@qq.com>
2020-01-04 11:13:20 +0800

    订单管理

--- src/pags/order/List.vue
+++ src/pags/order/List.vue
@@ -2,15 +2,25 @@
   <div>
     <h2>订单管理</h2>
     <!-- 按钮 -->
-    <el-button type="primary" size="small" @click="toAddHandler">添加</el-button> 
-    <el-button type="danger" size="small">批量删除</el-button>
+     <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="所有订单" name="first"></el-tab-pane>
+    <el-tab-pane label="待支付" name="second"></el-tab-pane>
+    <el-tab-pane label="待派单" name="third"></el-tab-pane>
+    <el-tab-pane label="待接单" name="fourth"></el-tab-pane>
+    <el-tab-pane label="待服务" name="fifth"></el-tab-pane>
+    <el-tab-pane label="待确认" name="sixth"></el-tab-pane>
+    <el-tab-pane label="已完成" name="seventh"></el-tab-pane>
+  </el-tabs>
     <!-- /按钮 -->
     <!-- 表格 -->
     <el-table :data="categories">
-        <el-table-column prop="id" label="编号"></el-table-column>
-        <el-table-column prop="name" label="栏目名称"></el-table-column>
-        <el-table-column prop="num" label="序号"></el-table-column>
-        <el-table-column prop="parentId" label="父栏目"></el-table-column>
+        <el-table-column prop="id" label="订单编号"></el-table-column>
+        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+        <el-table-column prop="total" label="总价"></el-table-column>
+        <el-table-column prop="status" label="状态"></el-table-column>
+        <el-table-column prop="customerId" label="顾客id"></el-table-column>
+        <el-table-column prop="waiterId" label="员工id"></el-table-column>
+        <el-table-column prop="addressId" label="服务地址"></el-table-column>
         <el-table-column label="操作">
             <template v-slot="slot">
             <a href="" @click.prevent="toDeleteHandler(slot.row.id)"><i class="el-icon-delete"></i></a>
@@ -20,12 +30,12 @@
     </el-table>
     <!-- /表格结束 -->
     <!-- 模态框 -->
-    <el-dialog
+    <!-- <el-dialog
       title="录入栏目信息"
       :visible.sync="visible"
       width="60%">
-        <!-- ---{{form}} -->
-      <el-form :model="form" label-width="80px">
+        < ---{{form}} -->
+      <!-- <el-form :model="form" label-width="80px">
         <el-form-item label="编号">
           <el-input v-model="form.id"></el-input>
         </el-form-item>
@@ -43,7 +53,7 @@
         <el-button size="small" @click="closeModalHandler">取 消</el-button>
         <el-button size="small" type="primary" @click="submitHandler">确 定</el-button>
       </span>
-    </el-dialog>
+    </el-dialog> --> -->
     <!-- /模态框 -->
   </div>
 </template>
@@ -55,9 +65,9 @@ export default {
   // 用于存放网页中需要调用的方法
   methods:{
     loadData(){
-      let url = "http://localhost:6677/category/findAll"
+      let url = "http://localhost:6677//order/findAll"
       request.get(url).then((response)=>{
-        // 将查询结果设置到customers中，this指向外部函数的this
+        // 将查询结果设置到order中，this指向外部函数的this
         this.categories = response.data;
       })
     },
@@ -67,7 +77,7 @@ export default {
       // request.post(url,this.form)
       // 查询字符串 type=customer&age=12
       // 通过request与后台进行交互，并且要携带参数
-      let url = "http://localhost:6677/category/saveOrUpdate";
+      let url = "http://localhost:6677/order/saveOrUpdate";
       request({
         url,
         method:"POST",
