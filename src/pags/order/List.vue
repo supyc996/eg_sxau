6d9998bce9edc0efd15ab0443348e24ec6ac5578
Sun9902 <1596459929@qq.com>
2020-01-04 15:49:09 +0800

    dingdan

--- src/pags/order/List.vue
+++ src/pags/order/List.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h2>订单管理</h2>
-    <!-- 按钮 -->
-     <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
     <el-tab-pane label="所有订单" name="first"></el-tab-pane>
     <el-tab-pane label="待支付" name="second"></el-tab-pane>
     <el-tab-pane label="待派单" name="third"></el-tab-pane>
@@ -13,48 +11,56 @@
   </el-tabs>
     <!-- /按钮 -->
     <!-- 表格 -->
-    <el-table :data="categories">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-        <el-table-column prop="waiterId" label="员工id"></el-table-column>
-        <el-table-column prop="addressId" label="服务地址"></el-table-column>
-        <el-table-column label="操作">
-            <template v-slot="slot">
-            <a href="" @click.prevent="toDeleteHandler(slot.row.id)"><i class="el-icon-delete"></i></a>
-            <a href="" @click.prevent="toUpdateHandler"><i class="el-icon-edit"></i></a>
-            </template>
-        </el-table-column>
+    <el-table :data="orders.list">
+      <el-table-column prop="id" label="编号"></el-table-column>
+      <el-table-column width="200" prop="orderTime" label="下单时间"></el-table-column>
+      <el-table-column prop="total" label="总价"></el-table-column>
+      <el-table-column prop="status" label="状态"></el-table-column>
+      <el-table-column prop="customerId" label="顾客ID"></el-table-column>
+      <el-table-column prop="waiterId" label="员工ID"></el-table-column>
+      <el-table-column prop="addressId" label="地址ID"></el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template v-slot="slot">
+          <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
+          <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
+        </template>
+      </el-table-column>
     </el-table>
     <!-- /表格结束 -->
+    <!-- 分页开始 -->
+    <el-pagination 
+        layout="prev, pager, next" 
+        :total="orders.total" 
+        @current-change="pageChageHandler">
+        </el-pagination>
+    <!-- /分页结束 -->
     <!-- 模态框 -->
-    <!-- <el-dialog
-      title="录入栏目信息"
+    <el-dialog
+      title="录入订单信息"
       :visible.sync="visible"
       width="60%">
-        < ---{{form}} -->
-      <!-- <el-form :model="form" label-width="80px">
-        <el-form-item label="编号">
-          <el-input v-model="form.id"></el-input>
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.username"></el-input>
         </el-form-item>
-        <el-form-item label="栏目名称">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="form.password"></el-input>
         </el-form-item>
-        <el-form-item label="序号">
-          <el-input v-model="form.num"></el-input>
+        <el-form-item label="真实姓名">
+          <el-input v-model="form.realname"></el-input>
         </el-form-item>
-        <el-form-item label="父栏目">
-          <el-input v-model="form.parentId"></el-input>
+        <el-form-item label="手机号">
+          <el-input v-model="form.telephone"></el-input>
         </el-form-item>
       </el-form>
+
       <span slot="footer" class="dialog-footer">
         <el-button size="small" @click="closeModalHandler">取 消</el-button>
         <el-button size="small" type="primary" @click="submitHandler">确 定</el-button>
       </span>
-    </el-dialog> --> -->
+    </el-dialog>
     <!-- /模态框 -->
+
   </div>
 </template>
 
@@ -64,18 +70,32 @@ import querystring from 'querystring'
 export default {
   // 用于存放网页中需要调用的方法
   methods:{
+    // 当分页中当前页改变的时候执行
+    pageChageHandler(page){
+        // 将params中当前页改为插件中的当前页
+        this.params.page = page-1;
+        // 加载
+        this.loadData();
+    },
     loadData(){
-      let url = "http://localhost:6677//order/findAll"
-      request.get(url).then((response)=>{
-        // 将查询结果设置到order中，this指向外部函数的this
-        this.categories = response.data;
+      let url = "http://localhost:6677/order/queryPage"
+      request({
+          url,
+          method:"post",
+          headers:{
+              "Content-Type":"application/x-www-form-urlencoded"
+          },
+          data:querystring.stringify(this.params)
+      }).then((response)=>{
+          // orders为一个对象，其中包含了分页信息，以及列表信息
+          this.orders = response.data;
       })
     },
     submitHandler(){
-      //this.form 对象 ---字符串--> 后台 {type:'customer',age:12}
-      // json字符串 '{"type":"customer","age":12}'
+      //this.form 对象 ---字符串--> 后台 {type:'order',age:12}
+      // json字符串 '{"type":"order","age":12}'
       // request.post(url,this.form)
-      // 查询字符串 type=customer&age=12
+      // 查询字符串 type=order&age=12
       // 通过request与后台进行交互，并且要携带参数
       let url = "http://localhost:6677/order/saveOrUpdate";
       request({
@@ -96,7 +116,6 @@ export default {
           message:response.message
         })
       })
-
     },
     toDeleteHandler(id){
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
@@ -104,20 +123,35 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.$message({
-          type: 'success',
-          message: '删除成功!'
-        });
+        // 调用后台接口，完成删除操作
+        let url = "http://localhost:6677/order/deleteById?id="+id;
+        request.get(url).then((response)=>{
+          //1. 刷新数据
+          this.loadData();
+          //2. 提示结果
+          this.$message({
+            type: 'success',
+            message: response.message
+          });
+        })
+        
+        
       })
       
     },
-    toUpdateHandler(){
+    toUpdateHandler(row){
+      // 模态框表单中显示出当前行的信息
+      this.form = row;
       this.visible = true;
     },
     closeModalHandler(){
       this.visible = false;
     },
     toAddHandler(){
+      // 将form变为初始值
+      this.form = {
+        type:"order"
+      }
       this.visible = true;
     }
   },
@@ -125,9 +159,13 @@ export default {
   data(){
     return {
       visible:false,
-      categories:[],
+      orders:{},
       form:{
-        type:"categories"
+        type:"order"
+      },
+      params:{
+          page:0,
+          pageSize:10
       }
     }
   },
